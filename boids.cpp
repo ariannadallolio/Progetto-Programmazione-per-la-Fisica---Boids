c929e09c1c33b9b5c47df40576ae99e58829ee1f
Arianna <ariannadallolio06@gmail.com>
2026-08-17 20:40:50 +0200

    funzione movimento + prova main

--- boids.cpp
+++ boids.cpp
@@ -55,7 +55,7 @@ std::vector<Velocity> generate_v(int n) {  // "cin n" in the main
 
   std::random_device r;  // seed
   std::default_random_engine eng{r()};
-  std::uniform_real_distribution<double> uniform{0.0, 1.0};  // max velocity?
+  std::uniform_real_distribution<double> uniform{0.0, 0.01};  // max velocity?
 
   std::generate_n(std::back_inserter(velocity), n, [eng, uniform]() mutable {
     const Velocity v{uniform(eng), uniform(eng)};
@@ -70,7 +70,7 @@ std::vector<Position> generate_p(int n) {  // "cin n" in the main
 
   std::random_device r;  // seed
   std::default_random_engine eng{r()};
-  std::uniform_real_distribution<double> uniform{0.0, 1.0};  // max position?
+  std::uniform_real_distribution<double> uniform{0.0, 0.2};  // max position?
 
   std::generate_n(std::back_inserter(position), n, [eng, uniform]() mutable {
     const Position p{uniform(eng), uniform(eng)};
@@ -81,9 +81,9 @@ std::vector<Position> generate_p(int n) {  // "cin n" in the main
 
 // la funzione neighbours_control vale per un boid solo, ritorna un vettore di
 // interi che corrispondono alle posizioni dei boids vicini al boid_to_check nel
-// vettore positions (quello con tutti i boids). Si potrebbe dare in input alle 3
-// funzioni delle velocità questo vettore così da non far scorrere tutte e 3 le
-// volte tutti i boid per trovarne i vicini. Poi (nel main?) si può fare un
+// vettore positions (quello con tutti i boids). Si potrebbe dare in input alle
+// 3 funzioni delle velocità questo vettore così da non far scorrere tutte e 3
+// le volte tutti i boid per trovarne i vicini. Poi (nel main?) si può fare un
 // ciclo che fa neighbours_control e le 3 funzioni per le velocità per ogni
 // boid. Quindi anche le 3 funzioni per le velocità possono essere rese per un
 // singolo boid, per poi fare un ciclo, anche perchè gli n che avevate messo
@@ -152,38 +152,88 @@ Velocity cohesion(double c, int boid_to_check,
 }
 // mettere if affinchè divisione non sia 0!
 
-
 // si potrebbero mettere i valori di input non nella classe ma in un file txt da
-// dare in input, e metterci forse vettori positions, velocities, neighbours e i 3 metodi per le velocità?
+// dare in input, e metterci forse vettori positions, velocities, neighbours e i
+// 3 metodi per le velocità?
 class boid {
   // valori da mettere in input
-  double a;
-  double c;
-  double d_s;
-  double d;
-  double s;
-  int n;  // numero di boids
+  double a = 0.2;
+  double c = 0.3;
+  double d_s = 0.09;
+  double d = 0.2;
+  double s = 0.6;
+  int n = 3;  // numero di boids
 
   // valori costanti
-  int ngen = 100;  // numero di reiterazioni (serve anche per la parte grafica)
-  std::vector<Velocity> velocities = generate_v(n);
-  std::vector<Position> positions = generate_p(n);
+  int ngen = 10;  // numero di reiterazioni (serve anche per la parte grafica)
+  double dt = 0.1;
 
+  // std::vector<Velocity> velocities = generate_v(n);
+  // std::vector<Position> positions = generate_p(n);
+
+  /*
   auto movimento() {
     for (int k = 0; k != ngen; ++k) {
       std::vector<Velocity> v1_vector = separation(s, d_s, n, positions);
     }
   }
+    */
+
+  auto print(std::vector<Velocity>& velocities,
+             std::vector<Position>& positions) {
+    for (int j = 0; j != n; ++j) {
+      std::cout << "Velocity: " << velocities[j].v_x << "," << velocities[j].v_y
+                << '\n';
+      std::cout << "Position: " << positions[j].x << "," << positions[j].y
+                << '\n';
+    }
+  }
+
+ public:
+  void movement(std::vector<Velocity>& velocities,
+                std::vector<Position>& positions) {
+    for (int k = 0; k <= ngen; ++k) {
+      std::vector<Velocity> updatedvelocity(n);
+      for (int j = 0; j != n; ++j) {
+        std::vector<int> boidvicini = neighbours_control(j, d, positions);
+        Velocity v1 = separation(s, d_s, j, boidvicini, positions);
+        Velocity v2 = alignment(a, j, boidvicini, velocities);
+        Velocity v3 = cohesion(c, j, boidvicini, positions);
+        Velocity vtot = velocities[j] + v1 + v2 + v3;
+        updatedvelocity[j] = vtot;
+      }
+      for (int j = 0; j != n; ++j) {
+        velocities[j] = updatedvelocity[j];
+        Position newp = {positions[j].x + dt * velocities[j].v_x,
+                         positions[j].y + dt * velocities[j].v_y};
+
+        positions[j] = newp;
+      }
+      print(velocities, positions);
+    }
+  }
 };
+
+int main() {
+  int n{};
+  std::cout << "Quanti boids?" << '\n';
+  std::cin >> n;
+  std::vector<Velocity> velocities = generate_v(n);
+  std::vector<Position> positions = generate_p(n);
+
+  boid boid_prova{};
+  boid_prova.movement(velocities, positions);
+}
 
 // vettore posizione e vettore velocità
 // calcolo posizione del centro di massa stormo
 // ciclo for (su posizoni) per il calcolo di v1 e v3 con assert (distanze
-// devono essere minori di d) ciclo for (su velocità) per il calcolo di v2 con
-// assert (a deve essere <1)
+// devono essere minori di d) ciclo for (su velocità) per il calcolo di v2
+// con assert (a deve essere <1)
 
-// forse anche una funzione che ogni volta mi trova i boid a distanza <d e per
-// le 3 velocità mi usa nei calcoli solo quei boid senza dover scorrerli tutti
+// forse anche una funzione che ogni volta mi trova i boid a distanza <d e
+// per le 3 velocità mi usa nei calcoli solo quei boid senza dover scorrerli
+// tutti
 
 // per ogni velocità si somma quella "attuale" con v1, v2, v3
 // ricalcolo posizioni

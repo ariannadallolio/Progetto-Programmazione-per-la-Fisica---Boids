025bc75bde260e8d5a1ad3616825759e2667b0e1
Rebecca Calzolari <rebecca.calzolari@studio.unibo.it>
2026-08-24 17:14:47 +0200

    njkjkbj

--- statistics.hpp
+++ statistics.hpp
@@ -1,3 +1,19 @@
 #ifndef statistics_HPP
 #define statistics_HPP
+#include <vector>
+
+#include "boids.hpp"
+
+namespace pf {
+double mean_distance(std::vector<Boid> const& boid, double x_min, double x_max,
+                     double y_min, double y_max);
+double std_dev_distance(std::vector<Boid> const& boid, double const& mean,
+                        double x_min, double x_max, double y_min, double y_max);
+double mean_velocity(std::vector<Boid> const& boid);
+double std_dev_velocity(std::vector<Boid> const& boid, double const& mean);
+void print(std::vector<Boid> const& boid, double x_min, double x_max,
+           double y_min, double y_max);
+
+}  // namespace pf
+
 #endif

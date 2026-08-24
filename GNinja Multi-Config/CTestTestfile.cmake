b4bee00a7c6702b7778c1b68edf0bf064f18138b
Rebecca Calzolari <rebecca.calzolari@studio.unibo.it>
2026-08-24 19:01:49 +0200

    boh

--- GNinja Multi-Config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids
-# Build directory: /home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids/GNinja Multi-Config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

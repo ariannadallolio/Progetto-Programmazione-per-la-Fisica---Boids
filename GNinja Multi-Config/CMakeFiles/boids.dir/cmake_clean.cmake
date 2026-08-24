0e2de483800bf50ceab4b779e54c23f9935491b6
Arianna <ariannadallolio06@gmail.com>
2026-08-24 19:05:21 +0200

    aggiunta test cmake

---
+++ GNinja Multi-Config/CMakeFiles/boids.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boids.dir/boids.cpp.o"
+  "CMakeFiles/boids.dir/boids.cpp.o.d"
+  "CMakeFiles/boids.dir/flock.cpp.o"
+  "CMakeFiles/boids.dir/flock.cpp.o.d"
+  "CMakeFiles/boids.dir/main.cpp.o"
+  "CMakeFiles/boids.dir/main.cpp.o.d"
+  "CMakeFiles/boids.dir/statistics.cpp.o"
+  "CMakeFiles/boids.dir/statistics.cpp.o.d"
+  "boids"
+  "boids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GNinja Multi-Config/CMakeFiles/boids.t.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boids.t.dir/test.t.cpp.o"
+  "CMakeFiles/boids.t.dir/test.t.cpp.o.d"
+  "boids.t"
+  "boids.t.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boids.t.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

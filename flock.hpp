8c99c9a876cc1497e2560e168217524f44032228
Rebecca Calzolari <rebecca.calzolari@studio.unibo.it>
2026-08-25 11:51:28 +0200

    const

--- flock.hpp
+++ flock.hpp
@@ -54,9 +54,9 @@ class Flock {
  public:
   Flock(int n, Parameters const& par, Space const& space);
 
-  std::vector<Boid> const& boids();
-  Parameters const& parameters();
-  Space const& space();
+  std::vector<Boid> const& boids() const;
+  Parameters const& parameters() const;
+  Space const& space() const;
 
 
   void movement();

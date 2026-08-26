837febaec91e2751a533b7dc0b99cf592df2e98a
Rebecca Calzolari <rebecca.calzolari@studio.unibo.it>
2026-08-26 22:01:36 +0200

    predator movement update

--- flock.hpp
+++ flock.hpp
@@ -20,12 +20,12 @@ struct Parameters {
 };
 
 struct Predator_parameters {
-  double s_p;       // fattore di separazione dal predatore
-  double c_p;       // fattore di coesione del predatore
-  double d_chase;   // raggio di azione del predatore
-  double d_escape;  // raggio di percezione dei boid del predatore
-  double v_min_p;   // velocità predatore
-  double v_max_p;   // velocità predatore
+   double s_p = 0.10;        // fattore di separazione dal predatore
+  double c_p = 0.005;       // fattore di coesione del predatore
+  double d_chase = 300.0;   // raggio di azione del predatore
+  double d_escape = 120.0;  // raggio di percezione dei boid del predatore
+  double v_min_p = 7.0;     // velocita' minima predatore
+  double v_max_p = 14.0;    // velocita' massima predatore
 };
 
 void check_parameters(Parameters const& par, Space const& space);
@@ -48,6 +48,14 @@ Velocity cohesion(double c, int boid_to_check,
                   std::vector<int> const& neighbours,
                   std::vector<Boid> const& boids, Space const& space);
 
+std::vector<int> preys_control(double d_chase, Boid const& predator,
+                               std::vector<Boid> const& boids,
+                               Space const& space);
+Velocity chase(double c_p, Boid const& predator, std::vector<int> const& preys,
+               std::vector<Boid> const& boids, Space const& space);
+Velocity escape(double s_p, double d_escape, Boid const& boid,
+                Boid const& predator, Space const& space);
+
 Velocity limit_speed(double v_min, double v_max, Velocity v_tot);
 
 class Flock {
@@ -56,14 +64,19 @@ class Flock {
   // riempire nel main)
  private:
   Parameters par_;
+  Predator_parameters par_p_;
   Space space_;
   std::vector<Boid> boids_;
+  Boid predator_;
 
  public:
-  Flock(int n, Parameters const& par, Space const& space);
+  Flock(int n, Parameters const& par, Space const& space,
+        Predator_parameters const& par_p = Predator_parameters{});
 
-  std::vector<Boid> const& boids() const;
+std::vector<Boid> const& boids() const;
+  Boid const& predator() const;
   Parameters const& parameters() const;
+  Predator_parameters const& predator_parameters() const;
   Space const& space() const;
 
   void movement();

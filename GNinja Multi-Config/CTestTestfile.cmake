ecf520f6258e186af12e88e070e81ea3de0b6240
Rebecca Calzolari <rebecca.calzolari@studio.unibo.it>
2026-08-27 12:34:20 +0200

    rimuovi artefatti di build dal versionamento

--- GNinja Multi-Config/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids
-# Build directory: /home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids/GNinja Multi-Config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[boids.t]=] "/home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids/GNinja Multi-Config/boids.t")
-set_tests_properties([=[boids.t]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids/CMakeLists.txt;65;add_test;/home/ariannadallolio/pf_labs/boids/Progetto-Programmazione-per-la-Fisica---Boids/CMakeLists.txt;0;")

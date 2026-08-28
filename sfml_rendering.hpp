f5b0e9e6147d67fe16f103b2e9e1a332cca61b9d
Arianna <ariannadallolio06@gmail.com>
2026-08-28 17:29:37 +0200

    .

--- sfml_rendering.hpp
+++ sfml_rendering.hpp
@@ -3,6 +3,9 @@
 #include "flock.hpp"
 
 namespace pf {
+void sync_graphics(Flock& simulation_flock,
+                 std::vector<sf::ConvexShape>& boids_shapes,
+                 std::vector<sf::ConvexShape>& predator_shapes);
 void run_sfml(Flock& simulation_flock);
-}
+}  // namespace pf
 #endif
